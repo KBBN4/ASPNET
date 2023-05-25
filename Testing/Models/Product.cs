585120aa41ed8823a848d3bbdb22c218755381af
katherine Buendia <kbbuendia24@gmail.com>
2023-05-25 10:58:34 -0400

    ASPNET Update

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Testing.Models
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+
+		public object ProductID { get; set; }
+        public object Name{ get; set; }
+        public object Price { get; set; }
+        public object CategoryID { get; set; }
+        public object OnSale{ get; set; }
+        public object StockLevel { get; set; }
+
+    }
+}
+
