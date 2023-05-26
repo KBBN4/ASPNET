9a1873fd08ff8562c2d95e14a4eda88ae8c905b1
katherine Buendia <kbbuendia24@gmail.com>
2023-05-26 14:40:44 -0400

    Insert & Delete Products added

--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -13,9 +13,29 @@ namespace Testing.Models
             _conn = conn;
 		}
 
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
+        }
+
         public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
         }
 
         public Product GetProduct(int id)
@@ -23,7 +43,14 @@ namespace Testing.Models
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id;", new { id = id });
         }
 
-        
+        public void InsertProduct(Product productToInsert)
+        {
+            
+            
+                _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                    new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+            
+        }
 
         public void UpdateProduct(Product product)
         {
